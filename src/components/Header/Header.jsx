84d5e68cfb0245888ff69ffc3a920503e5b73231
Caleb Shade <pokesm4shvids@gmail.com>
2020-02-21 03:48:08 -0500

    Made the header of the website and layed ground work for the Landing page.
    - Made a header for the webpage which includes the logo, Games tab, Teams tab  and Login Page.
    
    - Added a global palette that will be used for all SCSS around the webpage.
    
    - Created the landing page files with no content in them yet.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import "./styles.scss";
+import logo from "../../assets/tvaa.png";
+
+class Header extends Component {
+    state = {};
+
+    render() {
+        return (
+                <nav>
+                    <div class="logo">
+                <a 
+                href="#home">
+                    <img 
+                    src={logo} 
+                    id="logo" />
+                </a>
+                </div>
+                <div class="boxes">
+                    <a id="nav" href="#games">GAMES</a>
+                    <a id="nav" href="#teams">TEAMS</a>
+                    <a id="nav" href="#login">LOG IN</a>
+                    </div>
+                </nav>
+        );
+    }
+}
+
+export default Header;
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./styles.scss";
+
+class Landing extends Component {
+    state = {};
+
+    render() {
+        return ();
+    }
+}
+
+export default Landing;
