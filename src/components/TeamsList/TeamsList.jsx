b271053b95cedb3f1bf4f71a730cd59742cd84f1
Caleb Shade <pokesm4shvids@gmail.com>
2020-05-19 00:37:01 -0400

    Added the fetch for teams on the TeamPage

--- src/components/TeamsList/TeamsList.jsx
+++ src/components/TeamsList/TeamsList.jsx
@@ -4,17 +4,31 @@ import teams from "../../assets/team.svg";
 import "./styles.scss";
 
 class TeamsList extends Component {
-    state = {};
+    state = {
+        listOfTeams: [],
+    };
+    componentWillMount() {
+        fetch("http://localhost:9000/teams", {method: 'GET'})
+        .then(result => result.json()
+         .then(data => this.setState((state, props) => ({ listOfTeams: data }))))
+    }
 
     render() {
+        const teamlist = this.state.listOfTeams;
+        const teamdivs = teamlist.map((teamkey, index) => 
+        <div key={teamkey.pk} id={teamkey.id} style={{backgroundColor: teamkey.color}}>
+            <img src={require(`${teamkey.image}`)} alt={teamkey.name} id={teamkey.id + "img"}/>
+        </div>
+        )
         return (
             <div className="teamscontainer">
                 <div className="teamsheader">
                     <div className="teamicon">
                         <SVG src={teams} />
                     </div>
-                    <p className="teamtitle">TEAMS</p>    
+                    <p className="teamtitle">TEAMS</p>
                 </div>
+                {teamdivs}
             </div>
         );
     }
